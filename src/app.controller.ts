ef8b7508e77e6c7ac285573ba540c4bb8a3d18ad
Dierlisson Justiniano <dierlisson1@gmail.com>
2025-06-02 23:35:46 -0300

    finish API RESTfull

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,31 +1,42 @@
-import { Controller, Get, Post, Query, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Query, Body, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Cliente } from './modelo/cliente';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getStatus(): string{
+  getStatus(): string {
     return "O node está rodando: " + new Date();
   }
 
   @Get("/clientes")
-  listarTodosClientes():Array<Cliente> {
+  listarTodosClientes(): Array<Cliente> {
     console.log("Entrou no método: listarTodosClientes");
     return this.appService.listarTodos();
   }
-  
+
   @Get("/cliente")
-  public buscarPorId(@Query('id') id:number): Cliente {
+  public buscarPorId(@Query('id') id: number): Cliente {
     console.log("Entrou no método: buscarPorId");
     return this.appService.buscarPorId(id);
   }
 
   @Post()
-  public salvar(@Body() cliente: Cliente):Cliente{
+  public salvar(@Body() cliente: Cliente): Cliente {
     console.log("Entrou no método: salvar");
     return this.appService.salvar(cliente);
+  }
+
+  @Put(':id')
+  public alterar(@Param('id') id: number, @Body() cliente: Cliente): Cliente {
+    console.log("Entrou no método: alterar");
+    return this.appService.atualizar(id, cliente);
+  }
+  @Delete(':id')
+  public excluir(@Param('id') id: number): void {
+    console.log("Entrou no método: excluir");
+    this.appService.excluir(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -4,26 +4,26 @@ import { Cliente } from './modelo/cliente';
 @Injectable()
 export class AppService {
   private listaClientes: Array<Cliente> = [];
-  constructor(){
+  constructor() {
     const client1 = new Cliente(1, 'Alice Almeida', 'alice@teste.com', 2010);
     const client2 = new Cliente(2, 'Carlos Alberto', 'carlos@teste.com', 2007);
     const client3 = new Cliente(3, 'Alice Almeida', 'alice@teste.com', 20);
     this.listaClientes.push(client1, client2, client3);
     console.log(this.listaClientes);
   }
-  public listarTodos(): Array<Cliente>{
+  public listarTodos(): Array<Cliente> {
     return this.listaClientes;
   }
-  public buscarPorId(id: number): Cliente{
+  public buscarPorId(id: number): Cliente {
     let cliente = this.listaClientes.find(
-    cliente => cliente.id == id
-  );
-  if (!cliente) {
-    throw new Error('Cliente não encontrado');
+      cliente => cliente.id == id
+    );
+    if (!cliente) {
+      throw new Error('Cliente não encontrado');
+    }
+    return cliente;
   }
-  return cliente;
-  }
-  public salvar(cliente:Cliente): Cliente{
+  public salvar(cliente: Cliente): Cliente {
     const incluirCliente = new Cliente(
       this.obterProximoId(),
       cliente.nome,
@@ -33,10 +33,26 @@ export class AppService {
     this.listaClientes.push(incluirCliente);
     return incluirCliente;
   }
-  public obterProximoId(): number{
-    if(this.listaClientes.length == 0){
+  public atualizar(id: number, cliente: Cliente): Cliente {
+    let indice = this.listaClientes.findIndex(
+      cliente => cliente.id == id
+    );
+    this.listaClientes[indice] = cliente;
+    return cliente;
+  }
+  public excluir(id: number): void {
+    let indice = this.listaClientes.findIndex(
+      cliente => cliente.id == id
+    );
+    if (indice === -1) {
+      throw new Error('Cliente não encontrado');
+    }
+    this.listaClientes.splice(indice, 1);
+  }
+  public obterProximoId(): number {
+    if (this.listaClientes.length == 0) {
       return 1
-    }else{
+    } else {
       let ultimoRegistro = this.listaClientes[
         this.listaClientes.length - 1
       ];
